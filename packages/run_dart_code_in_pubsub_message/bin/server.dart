945d392fc07f3ec2e2f7e8f6777cf1789ba1c774
Nick Meinhold <nick@enspyr.co>
2022-02-14 21:41:54 +1100

    [RDC] Decode message, eval code & update response

--- packages/run_dart_code_in_pubsub_message/bin/server.dart
+++ packages/run_dart_code_in_pubsub_message/bin/server.dart
@@ -1,15 +1,37 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:run_dart_code_in_pubsub_message/evaluate.dart';
+import 'package:run_dart_code_in_pubsub_message/typedefs.dart';
 import 'package:shelf/shelf.dart' show Request, Response;
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 Future<Response> handler(Request request) async {
   try {
-    String expression = '...'; // get this from pubsub message
+    var body = await request.readAsString();
+    var decodedBodyJson = jsonDecode(body);
+    print('body:\n$decodedBodyJson');
+
+    var messageJson = decodedBodyJson['message'];
+    var encodedMessageData = messageJson['data'];
+    final decodedMessageJson =
+        json.decode(utf8.decode(base64.decode(encodedMessageData))) as JsonMap;
+    print('decodedMessageJson: $decodedMessageJson');
+
+    // Get required values from the pubsub message
+    var expression = decodedMessageJson['value'] as String;
+    var applicationId = decodedMessageJson['application_id'] as String;
+    var token = decodedMessageJson['token'] as String;
 
     String result = await evaluate(expression);
+    print('result:\n$result');
 
-    // send a  pubsub message
-    print(result);
+    // Make a http call to edit the interaction response
+    var uri = Uri.parse(
+        "https://discord.com/api/v8/webhooks/$applicationId/$token/messages/@original");
+    var response = await http.patch(uri, body: {'content': result});
+
+    print('response:\n${response.body}');
 
     return Response.ok('...');
   } catch (e, s) {
